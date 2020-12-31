870981cce4b8e2246dc410341910c6f6aa3042e2
hilary139 <aghasilihilary@gmail.com>
2020-12-31 09:09:08 +0100

    first script

---
+++ index.py
@@ -0,0 +1,21 @@
+# this is my first python script
+
+#welcome to backend developement
+print('hello world')
+
+x = 'orange'
+y = '3'
+v , h , g = 'cherry' , 'fish' , 'bread'
+print(x)
+
+print(y)
+
+print(v)
+
+print(h)
+
+print(g)
+
+a = 'hilary'
+# a long sentence 
+print('my name is ' + a + " " + ' and i like eating' + " " + v + " " + h + " " + g)
